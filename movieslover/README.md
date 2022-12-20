eefe4604c8819f54c62e49bd080185da5ff469ce
ashutoshnishad24 <121117754+ashutoshnishad24@users.noreply.github.com>
2022-12-20 21:49:10 +0530

    Add files via upload

---
+++ movieslover/README.md
@@ -0,0 +1,4 @@
+# movieslover.github.io
+Online movies streaming website. Where you can watch stream 
+Movies Freely. Whithout showing any type of ads 
+you can watch movies🍿. 
---
+++ movieslover/js/ullu.js
@@ -0,0 +1,61 @@
+$(function () {
+    $('#header',).load('header.html');
+});
+$(function(){
+    $('#second_header',).load('second_header.html');
+})
+$(function(){
+    $('#footer',).load('footer.html');
+})
+
+
+
+let newsPro = document.getElementById('content');
+
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET','https://api.npoint.io/db1cebc29a11ba043a04',true);
+// xhr.open('GET','https://api.publicapis.org/entries',true);
+xhr.getResponseHeader('Content-type','application/json');
+
+xhr.onload= function(){
+    if(this.status === 200){
+       let json = JSON.parse(this.responseText);
+       let results = json.images;
+    //    console.log(results);
+       let newsHtml= "";
+       
+       results.forEach(function(element){
+        //   console.log(results[news]);
+           let news=`
+           <div class="card_second">
+                <div class="movie_det">
+                    <img src="${element["ImageURL"]}" alt="">
+                    <div class="all_det">
+                        <h4>${element["Name"]}</h4>
+                        <span class="year">${element["year"]}</span>
+                        <p>Rating <span>${element["Rating"]}/10</span></p>
+                        
+                        <div class="box">
+                            <a class="button" href="#popup1"><button style="width:100%;" id="${element["vid"]}" onClick="GFG_click(this.id)">Watch Now</button></a>
+                        </div>
+                    </div>
+                </div>
+           </div>`;
+            newsHtml += news;
+
+        });
+       newsPro.innerHTML = newsHtml;
+    }
+    else{
+        console.log("Error occured")
+    }
+}
+xhr.send()
+
+//video pop up
+
+function GFG_click(clicked){
+    document.getElementById("iframe").src
+    = "https://drive.google.com/file/d/"+clicked+"/preview";
+}
